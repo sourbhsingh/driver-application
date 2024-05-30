a8e89e4d6579a6fe2b7eb8d9aa82d164313edc61
jurgen-alfaro <dev.jurgen.alfaro@gmail.com>
2024-05-30 00:07:18 -0600

    feat: Created login props interface

---
+++ src/layout/Login/interface.ts
@@ -0,0 +1,14 @@
+export interface loginProps {
+    loginLogo: string;
+    loginLogoAltText: string;
+    loginHeader: string;
+    loginSlogan: string;
+    loginEyeIconOn: string;
+    loginEyeIconOff: string;
+    loginUsernamePlaceholder: string;
+    loginPasswordPlaceholder: string;
+    loginButtonText: string;
+    loginHidePasswordAltText: string;
+    loginShowPasswordAltText: string;
+  }
+  
