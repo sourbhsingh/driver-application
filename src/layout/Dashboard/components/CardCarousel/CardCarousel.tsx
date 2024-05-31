c66ff5d85328723e327a404c14f67c066de84955
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2024-05-30 20:00:14 -0400

    fix: adjusted the height of the notification cards

--- src/layout/Dashboard/components/CardCarousel/CardCarousel.tsx
+++ src/layout/Dashboard/components/CardCarousel/CardCarousel.tsx
@@ -5,7 +5,7 @@ export default function CardCarousel(props: CardCarouselInterface) {
   const { children } = props;
 
   return (
-    <div className='w-full max-w-full p-4 h-80 space-x-4 bg-neutral rounded-box overflow-x-auto'>
+    <div className='w-full max-w-full p-4 h-auto space-x-4 bg-neutral rounded-box overflow-x-auto'>
       <div className='flex space-x-4 h-72 '>
         {React.Children.map(children, (child, index) => (
           <div className='carousel-item flex-shrink-0' key={index}>
